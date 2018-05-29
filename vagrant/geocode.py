fada9f5b840fb7c7c751a90097c0ed3e7c34ff21
Josh Breckenridge <bloo1987@gmail.com>
2018-05-29 19:44:35 -0400

    Added functionality to make request to Google geocode API

--- vagrant/geocode.py
+++ vagrant/geocode.py
@@ -4,12 +4,12 @@ import requests
 
 def get_coordinates(input_string):
     """Takes an input string of a location
-    and returns the long/lat coordinates of the 
-    location"""
+    and returns the long/lat coordinates of the
+    location."""
 
-    # Try and retrieve client_id from file
+    # Try and retrieve client_id from file.
     # Used to keep the client_id from making it
-    # into version control
+    # into version control.
     try:
         with open('g_client_id') as client_id:
             g_id = client_id.read()
@@ -18,3 +18,13 @@ def get_coordinates(input_string):
     else:
         print(g_id)
 
+    # Create the URL for API request
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = {
+        'address': input_string,
+        'key': g_id
+    }
+
+    # Use requests library to send request to Google API
+    response = requests.get(url=base_url, params=params).json()
+    print(response)
