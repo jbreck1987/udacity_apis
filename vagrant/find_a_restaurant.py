9c6d94118cd7565bed11fc6d7d71f1770b52689d
Josh Breckenridge <bloo1987@gmail.com>
2018-06-11 18:32:51 -0400

    Added functionality to get image info for first venue

--- vagrant/find_a_restaurant.py
+++ vagrant/find_a_restaurant.py
@@ -9,6 +9,7 @@ import requests
 foursquare_client_id = open_id_file('fsq_client_id')
 foursquare_client_secret = open_id_file('fsq_client_secret')
 foursquare_version = '20180505'
+foursquare_photo_uri = 'https://api.foursquare.com/v2/venues/{}/photos'
 
 
 def find_restaurant(meal_type, location):
@@ -30,22 +31,31 @@ def find_restaurant(meal_type, location):
         }
         response = requests.get(url=search_uri, params=search_params).json()
     else:
-        print("oops, didnt have any coordinates!")
+        print("Oops, didnt have any coordinates!")
+        return None
 
     # Grab the first restaurant from response
     if response['response']['venues']:
         restaurant = {
             'name': response['response']['venues'][0]['name'],
-            'id': response['response']['venues'][0]['categories'][0]['id']
+            'id': response['response']['venues'][0]['id']
         }
-        return restaurant
     else:
         print("Oops, didnt find anything!")
+        return None
 
-        # 4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-        # 5. Grab the first image
-        # 6. If no image is available, insert default a image url
-        # 7. Return a dictionary containing the restaurant name, address, and image url
+    # Create URL to get image information for the restaurant
+    search_uri = foursquare_photo_uri.format(restaurant['id'])
+    search_params = {
+        'client_id': foursquare_client_id,
+        'client_secret': foursquare_client_secret,
+        'v': foursquare_version
+    }
+    response = requests.get(url=search_uri, params=search_params).json()
+    return response
+    # 5. Grab the first image
+    # 6. If no image is available, insert default a image url
+    # 7. Return a dictionary containing the restaurant name, address, and image url
 # if __name__ == '__main__':
 #   find_restaurant('Pizza', 'Tokyo, Japan')
 #   find_restaurant('Tacos', 'Jakarta, Indonesia')
